dead7341104d69935406b3f69ad03ea8116404a7
Anthony GONÇALVES <anthony.goncalves@fms-ea.com>
2024-09-20 10:57:15 +0200

    Adding main menu & language switch

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /*
  * @author A. GONÇALVES
@@ -25,8 +26,10 @@ public class MainClass {
 	// "M" stands for Military Transport,
 	// "B" stands for Business Planes.
 
+	private static Scanner scan = new Scanner(System.in);
 	public static HashMap<String, HashMap<String, String>> planes = new HashMap<String, HashMap<String, String>>();
 	private static ArrayList<String> planesToAdd = new ArrayList<String>();
+	private static boolean frenchMode = false;
 
 	public static void main(String[] args) {
 		planesToAdd.add("FA3200-000001");
@@ -38,8 +41,63 @@ public class MainClass {
 
 		for (String plane : planesToAdd)
 			AddIncomingPlaneByID(plane);
+
+		int menuChoice = MainMenu(frenchMode);
+
+		while (menuChoice != 6) {
+			switch (menuChoice) {
+			case 1:
+				DisplayAllPlanes();
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+				frenchMode = !frenchMode;
+				break;
+			default:
+				break;
+			}
+
+			menuChoice = MainMenu(frenchMode);
+		}
+
+		System.out.println(frenchMode ? "À bientôt." : "Goodbye.");
+	}
+
+	// Displays the main menu
+	private static int MainMenu(boolean french) {
+		System.out.println("\n\n\n\n");
 		
-		DisplayAllPlanes();
+		if (french)
+			System.out.println(
+					"Bonjour, user 816409.\n\nMENU PRINCIPAL - Entrez votre choix.\n-------------------------------------------------------------------------------\n"
+							+ "1. Afficher tous les avions\n" + "2. Rechercher un avion par mot-clé programme\n"
+							+ "3. Ajouter ou supprimer une pièce sur un avion\n" + "4. Afficher le détail d'un avion\n"
+							+ "5. Switch to English\n" + "6. Quitter");
+		else
+			System.out.println(
+					"Welcome, user 816409.\n\nMAIN MENU - Please enter your choice.\n-------------------------------------------------------------------------------\n"
+							+ "1. Display all planes\n" + "2. Search for a plane by program keyword\n"
+							+ "3. Add or remove a part to/from a plane\n" + "4. Display detailled view about a plane\n"
+							+ "5. Passer au Français\n" + "6. Exit");
+
+		int result = 0;
+
+		while (result < 1 || result > 6) {
+			if (!scan.hasNextInt())
+				scan.next();
+			else
+				result = scan.nextInt();
+		}
+
+		return result;
 	}
 
 	// Adds a new plane after checking its ID and authenticity
@@ -60,7 +118,7 @@ public class MainClass {
 
 	// Displays all planes in database
 	private static void DisplayAllPlanes() {
-		System.out.println("PROGRAM :     PLANE TYPE :              GENERATION :      ACTUAL STATE :        FULL ID :");
+		System.out.println(frenchMode ? "PROGRAMME :   TYPE D'AVION :            GÉNÉRATION :      ÉTAT ACTUEL :         IDENTIFIANT :" : "PROGRAM :     PLANE TYPE :              GENERATION :      ACTUAL STATE :        FULL ID :");
 
 		// Formats each plane's data for further displaying
 		for (Map.Entry<String, HashMap<String, String>> plane : planes.entrySet()) {
@@ -70,9 +128,8 @@ public class MainClass {
 			planeInfo.put("fullID", planeKey);
 			planeInfo.put("generation", ((String) planeInfo.get("program")).substring(0, 1) + "              ");
 
-			planeInfo.put("programDisplayed",
-					planeInfo.get("program").substring(0, 4)
-							+ (planeInfo.get("program").substring(4, 5).equals("M") ? "M" : " "));
+			planeInfo.put("programDisplayed", planeInfo.get("program").substring(0, 4)
+					+ (planeInfo.get("program").substring(4, 5).equals("M") ? "M" : " "));
 
 			switch (planeInfo.get("type")) {
 			case "F":
