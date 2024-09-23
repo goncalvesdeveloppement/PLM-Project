9dcc79657b6f053828000eff54dce4973226b619
Anthony GONÇALVES <anthony.goncalves@fms-ea.com>
2024-09-23 14:56:25 +0200

    Exercice 2, début du 3 et optimisations

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +32,8 @@ public class MainClass {
 	private static ArrayList<String> planesToAdd = new ArrayList<String>();
 	private static boolean frenchMode = false;
 
+	public static HashMap<Integer, ArrayList<String>> parts = new HashMap<Integer, ArrayList<String>>();
+
 	public static void main(String[] args) {
 		Initialize();
 
@@ -39,13 +42,32 @@ public class MainClass {
 		while (menuChoice != 6) {
 			switch (menuChoice) {
 			case 1:
-				DisplayAllPlanes();
+				DisplayAllPlanes("");
 				break;
 			case 2:
-
+				System.out.println(frenchMode ? "Entrez un mot-clé de recherche :" : "Enter a search keyword");
+				String keyword = scan.next();
+				DisplayAllPlanes(keyword);
 				break;
 			case 3:
+				int partMenuChoice = PartMenu(frenchMode);
 
+				while (partMenuChoice != 3) {
+					switch (partMenuChoice) {
+					case 1:
+						DisplayAllParts();
+						break;
+					case 2:
+						// Select a plane
+						break;
+					case 3:
+						break;
+					default:
+						break;
+					}
+
+					partMenuChoice = PartMenu(frenchMode);
+				}
 				break;
 			case 4:
 
@@ -66,7 +88,7 @@ public class MainClass {
 	// Displays the main menu
 	private static int MainMenu(boolean french) {
 		System.out.println("\n\n\n\n");
-		
+
 		if (french)
 			System.out.println(
 					"Bonjour, user 816409.\n\nMENU PRINCIPAL - Entrez votre choix.\n-------------------------------------------------------------------------------\n"
@@ -79,6 +101,33 @@ public class MainClass {
 							+ "1. Display all planes\n" + "2. Search for a plane by program keyword\n"
 							+ "3. Add or remove a part to/from a plane\n" + "4. Display detailled view about a plane\n"
 							+ "5. Passer au Français\n" + "6. Exit");
+
+		int result = 0;
+
+		while (result < 1 || result > 6) {
+			if (!scan.hasNextInt())
+				scan.next();
+			else
+				result = scan.nextInt();
+		}
+
+		return result;
+	}
+
+	// Displays the part menu
+	private static int PartMenu(boolean french) {
+		System.out.println("\n\n\n\n");
+
+		if (french)
+			System.out.println(
+					"Bienvenue dans la boutique de customisation.\n\nACCUEIL CUSTOM - Entrez votre choix.\n-------------------------------------------------------------------------------\n"
+							+ "1. Lister les pièces disponibles\n" + "2. Sélectionner un avion pour modification\n"
+							+ "3. Retourner au menu principal");
+		else
+			System.out.println(
+					"Welcome to customs center.\n\nCUSTOMS HOME - Please enter your choice.\n-------------------------------------------------------------------------------\n"
+							+ "1. Display all available parts\n" + "2. Select a plane to custom\n"
+							+ "3. Back to main menu");
 
 		int result = 0;
 
@@ -108,15 +157,31 @@ public class MainClass {
 			return false;
 	}
 
-	// Displays all planes in database
-	private static void DisplayAllPlanes() {
-		System.out.println(frenchMode ? "PROGRAMME :   TYPE D'AVION :            GÉNÉRATION :      ÉTAT ACTUEL :         IDENTIFIANT :" : "PROGRAM :     PLANE TYPE :              GENERATION :      ACTUAL STATE :        FULL ID :");
+	// Add a new part
+	private static void AddIncomingPart(String name, String category, String price) {
+		int newID = parts.size() + 1;
+
+		String[] arr = { name, category, price };
+		parts.put(newID, new ArrayList<String>(Arrays.asList(arr)));
+	}
+
+	// Displays all planes in database (optionnally planes matching a specific
+	// program keyword)
+	private static void DisplayAllPlanes(String keyword) {
+		System.out.println(frenchMode
+				? "PROGRAMME :   TYPE D'AVION :            GÉNÉRATION :      ÉTAT ACTUEL :         IDENTIFIANT :"
+				: "PROGRAM :     PLANE TYPE :              GENERATION :      ACTUAL STATE :        FULL ID :");
+		boolean planesFound = false;
 
 		// Formats each plane's data for further displaying
 		for (Map.Entry<String, ArrayList<String>> plane : planes.entrySet()) {
 			String planeKey = plane.getKey();
 			ArrayList<String> planeInfo = plane.getValue();
 
+			if (keyword != "" && !planeInfo.get(0).substring(0, 5).contains(keyword))
+				continue;
+
+			planesFound = true;
 			planeInfo.add(planeKey); // 4 : Full ID
 			planeInfo.add(((String) planeInfo.get(0)).substring(0, 1) + "              "); // 5 : Generation
 
@@ -165,10 +230,26 @@ public class MainClass {
 				break;
 			}
 
-			System.out.println(planeInfo.get(6) + "         " + planeInfo.get(7) + "   "
-					+ planeInfo.get(5) + "   " + planeInfo.get(8) + "   "
-					+ planeInfo.get(4));
+			System.out.println(planeInfo.get(6) + "         " + planeInfo.get(7) + "   " + planeInfo.get(5) + "   "
+					+ planeInfo.get(8) + "   " + planeInfo.get(4));
 		}
+
+		if (!planesFound)
+			System.out.printf(
+					"\n\n" + (frenchMode ? "Aucun avion trouvé pour '%s'..." : "No plane found matching '%s '"),
+					keyword);
+	}
+
+	// Displays all available parts
+	private static void DisplayAllParts() {
+		System.out.println(frenchMode ? "ID PIECE   NOM            CATÉGORIE           PRIX"
+				: "PART ID    NAME           CATEGORY            PRICE");
+
+		parts.forEach((key, value) -> {
+			System.out.println(String.format("%-11s", key) + String.format("%-15s", value.get(0))
+					+ String.format("%-20s", value.get(1)) + String.format("%-8s", value.get(2)));
+		});
+
 	}
 
 	// Adds a set of parts to a specific plane
@@ -180,9 +261,10 @@ public class MainClass {
 
 	// Displays a detailed view about a specific plane
 	// private static void DisplayPlaneDetails(String planeID)...
-	
+
 	// Testing initialization
 	private static void Initialize() {
+		// Planes adding
 		planesToAdd.add("FA3200-000001");
 		planesToAdd.add("TD400M-972137");
 		planesToAdd.add("TA3800-752820");
@@ -191,6 +273,14 @@ public class MainClass {
 		planesToAdd.add("BA3000-432980");
 
 		for (String plane : planesToAdd)
-			AddIncomingPlaneByID(plane);		
+			AddIncomingPlaneByID(plane);
+
+		// Parts adding
+		AddIncomingPart("Compass", "navigation", "10000");
+		AddIncomingPart("GPS", "navigation", "25000");
+		AddIncomingPart("Anti-collide", "safety", "150000");
+		AddIncomingPart("Rockets", "conflicts", "300000");
+		AddIncomingPart("KeroZEN Boost", "mechanical", "775000");
+		AddIncomingPart("Climatisation", "comfort", "2500");
 	}
 }
